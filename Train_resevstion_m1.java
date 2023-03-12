e80cc30bf0a63ef8ad4c1b20d46747074c440d40
ManD-2113 <103494256+ManD-2113@users.noreply.github.com>
2023-03-12 15:12:23 +0530

    MILESTONE 1

--- Train_resevstion_m1.java
+++ Train_resevstion_m1.java
@@ -30,6 +30,11 @@ class Coches {
     List<Integer> seats_in_t3acCoches = new ArrayList<Integer>();
     List<Integer> seats_in_t2acCoches = new ArrayList<Integer>();
     List<Integer> seats_in_t1acCoches = new ArrayList<Integer>();
+
+    int totalseats_in_sliperCoch = 0;
+    int totalseats_in_t3acCoches = 0;
+    int totalseats_in_t2acCoches = 0;
+    int totalseats_in_t1acCoches = 0;
     Coches(){}
 
     void definingCoches(String s){
@@ -37,29 +42,48 @@ class Coches {
         if(s.charAt(0) == 'S' ){
             this.sliperCoches_no.add(coches_and_seats[0]);
             this.seats_in_sliperCoches.add(Integer.parseInt(coches_and_seats[1]));
+            totalseats_in_sliperCoch = totalseats(totalseats_in_sliperCoch, Integer.parseInt(coches_and_seats[1]));
         }
         else if(s.charAt(0) == 'B'){
             this.t3acCoches_no.add(coches_and_seats[0]);
             this.seats_in_t3acCoches.add(Integer.parseInt(coches_and_seats[1]));
+            totalseats_in_t3acCoches = totalseats(totalseats_in_t3acCoches, Integer.parseInt(coches_and_seats[1]));
         }
         else if(s.charAt(0) == 'A'){
             this.t2acCoches_no.add(coches_and_seats[0]);
             this.seats_in_t2acCoches.add(Integer.parseInt(coches_and_seats[1]));
+            totalseats_in_t2acCoches = totalseats(totalseats_in_t2acCoches, Integer.parseInt(coches_and_seats[1]));
         }
         else if(s.charAt(0) == 'H'){
             this.t1acCoches_no.add(coches_and_seats[0]);
             this.seats_in_t1acCoches.add(Integer.parseInt(coches_and_seats[1]));
+            totalseats_in_t1acCoches = totalseats(totalseats_in_t1acCoches, Integer.parseInt(coches_and_seats[1]));
         }
         else{
             this.train_No = Integer.parseInt(s);
         }
     }
+    int totalseats (int seats, int n){
+        seats = seats+n;
+        return seats;
+    }
     
+}
+
+class TicketBook {
+
+    String Date;
+    String type_of_couch;
+    int pnr;
+    int fair;
 }
 
 public class Train_resevstion_m1 {
     static List<Train_Data> cityData = new ArrayList<>();
     static List<Coches> cochesData= new ArrayList<>();
+    static List<TicketBook> ticketBookData = new ArrayList<>(); 
+    static int pnr = 100000001;
+    static int charge;
 
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
@@ -72,9 +96,6 @@ public class Train_resevstion_m1 {
         for (int i = 0; i < inpString.length; i++) {
             inpString[i] = inp.nextLine();
         }
-        // for (String string : inpString) {
-        //     System.out.println(string);
-        // }
 
 
         
@@ -99,26 +120,104 @@ public class Train_resevstion_m1 {
                 }
 
             }
-            System.out.println("----------------------------");
-            for (Train_Data t1 : cityData) {
-                System.out.println(t1.train_No+" "+t1.sourceCity+" "+t1.destinationCity+" "+t1.distance);
-            }
-            System.out.println("----------------------");
-            for (Coches c : cochesData) {
-                System.out.println(c.train_No);
-                printList(c.sliperCoches_no);
-                // printList(c.t1acCoches);
-                // printList(c.t2acCoches);
-                // printList(c.t3acCoches);
-            }
-            // String userInput = inp.nextLine();
-            // String [] inputarr = userInput.split(" ");
-        }
-        // inp.close();
-        static void printList(List<String> c){
-            for (String string : c) {
-                System.out.println(string);
-            }
-        }
-    }
+            while (true){
+                String userInput = inp.nextLine();
+                String [] inputarr = userInput.split(" ");
+                TicketBook t1 = new TicketBook();
+                Boolean route = isRoute(inputarr[0], inputarr[1]);
 
+                if(route){
+                    int distance =  findDistance(inputarr[0],inputarr[1]);
+
+                    if(inputarr[3].matches("SL")){
+                        charge = Integer.parseInt(inputarr[4])*distance*1;
+                        System.out.println(pnr+" "+ charge);
+                        pnr+=1;
+                    }
+                    else if (inputarr[3].matches("1A")){
+                        charge = Integer.parseInt(inputarr[4])*distance*2;
+                        System.out.println(pnr+" "+ charge);
+                        pnr+=1;
+                    }
+                    else if (inputarr[3].matches("2A")){
+                        charge = Integer.parseInt(inputarr[4])*distance*3;
+                        System.out.println(pnr+" "+ charge);
+                        pnr+=1;
+                    }
+                    else if (inputarr[3].matches("3A")){
+                        charge = Integer.parseInt(inputarr[4])*distance*4;
+                        System.out.println(pnr+" "+ charge);
+                        pnr+=1;
+                    }
+
+                }
+                else{
+                    System.out.println("No Trains Available");
+                }
+
+                }
+
+                
+                        
+        }
+            
+        static void booktickets(int trainno, int pnrno, int fair, String date){
+            for (Coches c : cochesData) {
+                if(c.train_No == trainno){
+                    TicketBook t = new TicketBook();
+                    t.Date = date;
+                    t.pnr  = pnrno;
+                    t.fair = fair;
+
+                }
+            }
+        }
+
+        static int findDistance(String s, String d){
+            for (Train_Data train_Data : cityData) {
+                if(train_Data.sourceCity.matches(s)&&train_Data.destinationCity.matches(d)){
+                    return train_Data.distance;
+                }   
+            }
+            return 0;
+        }
+
+
+            static int avialableSeat (int train, String couch, int passenger){
+                    for (Coches c : cochesData) {
+                        if(c.train_No == train){
+                            if (couch.matches("SL")){
+                                int availableSeats = c.totalseats_in_sliperCoch;
+                                c.totalseats_in_sliperCoch = c.totalseats_in_sliperCoch - passenger;
+                                return availableSeats;
+                            }
+                            else if (couch.matches("1A")) {
+                                int availableSeats = c.totalseats_in_t1acCoches;
+                                c.totalseats_in_t1acCoches = c.totalseats_in_t1acCoches - passenger;
+                                return availableSeats; 
+                            }
+                            else if (couch.matches("2A")) {
+                                int availableSeats = c.totalseats_in_t2acCoches;
+                                c.totalseats_in_t2acCoches = c.totalseats_in_t2acCoches - passenger;
+                                return availableSeats;
+                            }
+                            else {
+                                int availableSeats = c.totalseats_in_t3acCoches;
+                                c.totalseats_in_t3acCoches = c.totalseats_in_t3acCoches - passenger;
+                                return availableSeats;
+                            }
+                        }
+                    }
+
+                    return 0;
+            }
+
+            static boolean isRoute(String scity, String Dcity){
+                for (Train_Data train_Data : cityData) {
+                    if(train_Data.sourceCity.matches(scity)&&train_Data.destinationCity.matches(Dcity)){
+                        return true;
+                    }    
+                }
+                return false;
+            }
+    }
